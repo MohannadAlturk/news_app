1049809f80d2d5d663567f352d96c7ee7f5e9cd7
SimeonSimeonov24 <114754165+SimeonSimeonov24@users.noreply.github.com>
2024-10-17 11:49:48 +0200

    integrated News API and test display within news_screen.dart (#8)
    Added .env for variables such as API keys

---
+++ lib/services/news_api_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class NewsApiService {
+  final String _baseUrl = 'https://newsapi.org/v2';
+  final String? apiKey = dotenv.env['NEWS_API_KEY']; // Fetch API key from .env
+
+  // Fetch top headlines based on a category
+  Future<List<dynamic>> fetchTopHeadlines({String category = 'general', String country = 'us'}) async {
+    final url = Uri.parse('$_baseUrl/top-headlines?country=$country&category=$category&apiKey=$apiKey');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data['articles']; // Return the articles
+    } else {
+      throw Exception('Failed to fetch news');
+    }
+  }
+
+  // Fetch articles based on search query
+  Future<List<dynamic>> fetchArticlesByQuery(String query) async {
+    final url = Uri.parse('$_baseUrl/everything?q=$query&apiKey=$apiKey');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data['articles']; // Return the articles
+    } else {
+      throw Exception('Failed to fetch news');
+    }
+  }
+}
