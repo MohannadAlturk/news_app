c210e7f5d829f875d7165aab6215575de65ab04c
SimeonSimeonov24 <114754165+SimeonSimeonov24@users.noreply.github.com>
2024-12-04 21:29:37 +0100

    implemented search screen, added translations, changes to navbar

--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:news_app/screens/favorites_screen.dart';
 import 'package:news_app/screens/news_screen.dart';
 import 'package:news_app/screens/settings_screen.dart';
+import 'package:news_app/screens/search_screen.dart';
+import 'package:news_app/services/language_service.dart';
 
 class BottomNavBar extends StatelessWidget {
   final int currentIndex;
@@ -9,6 +11,7 @@ class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key, this.currentIndex = 0});
 
   void _onItemTapped(BuildContext context, int index) {
+    if (index == currentIndex) return; // Avoid reloading the same page
     switch (index) {
       case 0:
         Navigator.pushReplacement(
@@ -28,24 +31,39 @@ class BottomNavBar extends StatelessWidget {
           MaterialPageRoute(builder: (context) => const SettingsScreen()),
         );
         break;
+      case 3:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const SearchScreen()),
+        );
+        break;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      backgroundColor: Colors.white, // Set background color to white
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.menu), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
+      items:  [
+        BottomNavigationBarItem(icon: Icon(Icons.menu), label: getTranslatedText("news")),
+        BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: getTranslatedText("favorites")),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: getTranslatedText("settings")),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: getTranslatedText("search")),
       ],
       currentIndex: currentIndex,
+      backgroundColor: Colors.white, // Ensure uniform background color
+      //elevation: 0, // Remove shadow for a clean look
       selectedItemColor: Colors.blue,
       unselectedItemColor: Colors.black45,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
+      selectedIconTheme: const IconThemeData(size: 24),
+      unselectedIconTheme: const IconThemeData(size: 24),
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
       onTap: (index) => _onItemTapped(context, index),
+      type: BottomNavigationBarType.fixed, // Prevent shifting behavior
     );
+  }
+
+  String getTranslatedText(String key) {
+    return LanguageService.translate(key);
   }
 }
